f86906e4aaa9fef8771622c5989e5f42516024a8
DFNero <abbyansyahatha@gmail.com>
2025-10-10 20:27:01 +0700

    fix

---
+++ backend/src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { router as produkRoutes } from "./routes/produkRoutes";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/produk", produkRoutes);
+
+export default app;
