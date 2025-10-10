2dda7c765d91c3d50dd148490894690a1ee3663a
DFNero <abbyansyahatha@gmail.com>
2025-10-10 20:44:10 +0700

    debuggin databse await

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { router as produkRoutes } from "./routes/produkRoutes";
+import produkRoutes from "./routes/produkRoutes"; // ✅ default import
 
 dotenv.config();
-const app = express();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
